fd57653887730da8a8bf352b0122e5c486b4c245
susmithasubramaniam <susmithasubramaniam@gmail.com>
2017-06-17 17:00:30 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Largest {
+	public static void main (String[] args) {
+		 int a=0,b=0,c=0;
+            int largest=0;
+            //Scanner class to take user input.
+            Scanner X = new Scanner(System.in);
+            a = X.nextInt(); //read integer number
+            b = X.nextInt(); //read integer number
+            c = X.nextInt(); //read integer number
+             
+            if( a>b && a> c)
+                largest = a;
+            else if(b>a && b>c)
+                largest = b;
+            else
+                largest = c;
+             
+            System.out.println("Lagest Number is : "+largest);
+	}
+}
